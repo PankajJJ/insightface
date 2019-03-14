fd8f1b8494051631f0d20df2b2b91ad9feed67b1
penolove <chinghua.yang@appier.com>
2019-03-14 23:26:33 +0800

    add model store and model restore mechanism

--- arcface_objects_classifier.py
+++ arcface_objects_classifier.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 
 import arrow
 import numpy as np
@@ -14,11 +15,20 @@ from deploy import face_model
 
 
 class ArcFaceClassifier(ObjectClassifier):
-    def __init__(self, args, objects_frame, registered_ids):
+    def __init__(self, args, registered_ids,
+                 objects_frame=None, registered_images_embedding=None,
+                 threshold=0.0):
+        assert objects_frame is not None or registered_images_embedding is not None
         self.model = face_model.FaceModel(args)
         self.image_size = [int(i) for i in args.image_size.split(',')]
-        self.registed_images = self.model.get_faces_feature(objects_frame)
+        if registered_images_embedding:
+            self.registered_images_embedding = registered_images_embedding
+        else:
+            self.registered_images_embedding = self.model.get_faces_feature(objects_frame)
+
         self.registered_ids = registered_ids
+        self.threshold = threshold
+        self.unknown = 'unknown'
 
     def detect(self, image_obj, bbox_objs=None):
         if bbox_objs:
@@ -33,13 +43,15 @@ class ArcFaceClassifier(ObjectClassifier):
 
         objects_frame = np.transpose(objects_frame, (0, 3, 1, 2))
         objects_embedding = self.model.get_faces_feature(objects_frame)
-        similar_matrix = objects_embedding.dot(self.registed_images.T)
+        similar_matrix = objects_embedding.dot(self.registered_images_embedding.T)
         detected_idx = similar_matrix.argmax(1)
         result_objects = []
         for idx, bbox in enumerate(bbox_objs):
             x1, y1, x2, y2, _, _, _ = bbox
             label = self.registered_ids[detected_idx[idx]]
             score = similar_matrix[idx, detected_idx[idx]]
+            if score < self.threshold:
+                label = self.unknown
             result_objects.append(BoundedBoxObject(x1, y1, x2, y2, label, score, ''))
 
         image_dict = {
@@ -50,7 +62,16 @@ class ArcFaceClassifier(ObjectClassifier):
         return detection_result
 
     def valid_labels(self):
-        return set(self.registered_ids)
+        return set(self.registered_ids + [self.unknown])
+
+    def store_embedding_info(self, pkl_path):
+        with open(pkl_path, 'wb') as f:
+            pickle.dumps((self.registered_images_embedding, self.registered_ids), file=f)
+
+    @staticmethod
+    def restore_embedding_info(pkl_path):
+        with open(pkl_path, 'rb') as f:
+            return pickle.loads(f)
 
 
 if __name__ == '__main__':
@@ -78,7 +99,7 @@ if __name__ == '__main__':
     objects_frame = np.transpose(objects_frame, (0, 3, 1, 2))
     registered_ids = [i.label for i in register_image_bbox_objs]
 
-    arcface_classifier = ArcFaceClassifier(args, objects_frame, registered_ids)
+    arcface_classifier = ArcFaceClassifier(args, registered_ids, objects_frame=objects_frame)
 
     test_image_bbox_objs = [
         BoundedBoxObject(x1=367, y1=101, x2=412, y2=160, label='face', score=1, meta='1'),
@@ -107,3 +128,7 @@ if __name__ == '__main__':
 
     ImageHandler.draw_bbox(test_image_obj.pil_image_obj, detection_result.detected_objects)
     ImageHandler.save(test_image_obj.pil_image_obj, "detected_image/drawn_image_2.jpg")
+
+    arcface_classifier.store_embedding_info('183_model.pkl')
+    embedding, ids = ArcFaceClassifier.restore_embedding_info('183_model.pkl')
+    print(embedding.shape)
